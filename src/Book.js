16ceac1a52a908fbbb748a7015f906f5b05048e2
Dariusz Głażewski <szprx1910@gmail.com>
2022-05-18 22:27:52 +0200

    wyswietlanie wszytskich ksaizek

--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,23 @@
 import React, {Component} from "react";
 import "./Book.css"
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import DetailsBookPage from "./page/DetailsBookPage";
 
 class Book extends Component {
-    title;
+    id = this.props.info.id;
+    title = this.props.info.title;
 
     render() {
         return (
             <div>
-                <ul>
-                    <li> {this.props.info.title}</li>
-                </ul>
+                <Router>
+                        <li>
+                            <Link to="/bookDetails">{this.title}</Link>
+                            <Route path="/bookDetails" exact component={DetailsBookPage(this.id)}></Route>
+                        </li>
+                </Router>
             </div>
         );
     }
 }
-
 export default Book;
---
+++ src/page/BooksPage.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import Book from "../Book";
+
+class BooksPageData extends Component {
+
+    state = {
+        data: [],
+        name: "daro"
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/book/all')
+            .then(response => response.json())
+            .then(data => {
+                    console.log(data);
+                    this.setState({data})
+                }
+            );
+    }
+
+    render() {
+        return (<div>
+            <ul>{this.state.data.map(book => <Book info={book}/>)}</ul>
+        </div>);
+    }
+}
+
+export default BooksPageData;
